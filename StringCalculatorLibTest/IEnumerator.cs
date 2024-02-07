09cab1901c945642daa246795fc81301e08e8476
Ishanmadan10 <156063462+Ishanmadan10@users.noreply.github.com>
2024-02-07 15:39:53 +0530

    Add files via upload

---
+++ StringCalculatorLibTest/IEnumerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting1.Tests
+{
+    public class StringCalcTestInputData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "", 0 };
+            yield return new object[] { "1, 2", 3 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
